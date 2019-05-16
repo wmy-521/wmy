ee1c0dbd19d211807264e479226d37f46d43f738
123456 <2686460170@qq.com>
2019-05-16 08:52:39 +0800

    516

---
+++ rk516.php
@@ -0,0 +1,15 @@
+<?php
+$n=1;
+$m=13;
+print_r(num($n,$m));
+function num($n,$m){
+    for ($i=0;$i<$y;$i++){
+        $data[]=$y;
+        if($n==$y){
+            $data[]= substr_count($y);
+        }
+    }
+    return array_sum($n);
+}
+
+?>
