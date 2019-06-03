c25f66d56588470adde347d4531dcf55772ed819
123456 <2686460170@qq.com>
2019-06-03 08:59:42 +0800

    603

---
+++ rk603.php
@@ -0,0 +1,19 @@
+<?php
+$a=1;
+$b=1;
+$str1=[];
+$str2=[];
+if($a=1){
+    $count1=count($a);
+    $count1=$str;
+    echo '--';
+}
+if($b){
+    $count2=count($b);
+    $count2=$str;
+    echo '--';
+}
+
+
+
+?>
